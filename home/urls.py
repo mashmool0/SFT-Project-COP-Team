00034adfbde412825552ffc01754da5518c6321f
Amir Mashayekhi <ap4games2017@gmail.com>
2024-12-12 08:06:29 +0330

    add some changes to front of project home

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
-from django.urls import  path
-from .views import
+from django.urls import path
+from .views import home_view
 
+app_name = "home"
 urlpatterns = [
-    path('',)
+    path('', home_view, name="home")
 ]
