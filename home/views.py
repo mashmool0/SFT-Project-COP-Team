ccf19a42e31eaf79a6b3198ca921595fb25f4976
Amir Mashayekhi <ap4games2017@gmail.com>
2024-12-14 21:17:59 +0330

    fix home.html and view of that

--- home/views.py
+++ home/views.py
@@ -2,7 +2,29 @@
 from django.shortcuts import render
 from .models import Event
 
+from django.core.paginator import Paginator
+
+from .models import Event, University
+
+# home/views.py
+from django.shortcuts import render
+from .models import Event
+
+from django.core.paginator import Paginator
+
+from .models import Event, University
+
 
 def home_view(request):
-    events = Event.objects.all()
-    return render(request, 'home/home.html', context={'events': events})
+    event_list = Event.objects.all()
+    universities = University.objects.all()  # بارگذاری دانشگاه‌ها
+    paginator = Paginator(event_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'events': page_obj,
+        'universities': universities,  # اضافه کردن دانشگاه‌ها به context
+        'page_obj': page_obj,
+    }
+    return render(request, 'home/home.html', context)
